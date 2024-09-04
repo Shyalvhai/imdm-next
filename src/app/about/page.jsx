a4aab75a0a196b8e86d8450d20d331cd44678e1e
Shyalvhai <avishekprajapati1@gmail.com>
2024-09-04 11:27:01 +1000

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,37 @@
+export default function about() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our IMDb clone, a modern platform where movie enthusiasts,
+        critics, and casual viewers come together to explore the world of
+        cinema. Our mission is to provide a comprehensive and user-friendly
+        database of films, TV shows, actors, directors, and more. Whether you're
+        searching for the latest blockbuster, rediscovering a classic, or diving
+        into the behind-the-scenes details of your favorite shows, our platform
+        is designed to offer an immersive and informative experience that caters
+        to all your entertainment needs.
+      </p>
+      <p>
+        We believe in the power of storytelling and aim to make discovering new
+        films and shows as enjoyable as watching them. Our platform offers
+        detailed information, including plot summaries, cast lists, user
+        ratings, and reviews. With personalized recommendations, trending lists,
+        and curated collections, finding your next favorite movie or TV show has
+        never been easier. Dive deep into our rich content, explore the works of
+        your favorite actors and directors, and connect with a community of
+        fellow cinephiles who share your passion for the silver screen.
+      </p>
+      <p>
+        More than just a database, our IMDb clone is a community-driven platform
+        where users can create accounts, rate films, write reviews, and
+        contribute to discussions about the latest releases and timeless
+        classics. We strive to foster a welcoming environment for all movie
+        lovers, from the casual viewer to the dedicated cinephile. Join us and
+        be part of a growing community that celebrates the art of filmmaking,
+        where your opinions matter, and your voice can help guide others in
+        their cinematic journey.
+      </p>
+    </div>
+  );
+}
